95df0e10ce40308ba481158f35fd9bdb773a4378
guadalupecosta <guadalupecostam@gmail.com>
2023-07-31 18:56:51 +0200

    First commit

---
+++ clase1/js/main.js
@@ -0,0 +1,66 @@
+/* let nombre = "Guadalupe" // string
+let apellido = "Costa"
+let edad = 23 // number
+
+const comision = "JavaScript"
+
+console.log("Datos: ", nombre, apellido, edad)
+
+// string: cadena de texto o alfanumericos
+let contrasenia = "123456gcm"
+
+// number: enteros y decimales
+let entero = 20
+let decimal = 1.5
+
+// operaciones aritmeticas
+
+let numeroA = 120
+let numeroB = 180
+
+// suma
+let sumaNumeros = numeroA + numeroB
+
+console.log("La suma de numeroA y numeroB es: ", sumaNumeros) // imprime directamente
+
+// resta
+let restaNumeros = numeroA - numeroB
+
+console.log("La resta de numeroA y numeroB es: ", restaNumeros)
+
+// multiplicación
+let multiplicacionNumeros = numeroA * numeroB
+
+console.log("La multiplicación de numeroA y numeroB es: ", multiplicacionNumeros)
+
+// división
+let divisionNumeros = numeroA / numeroB
+
+console.log("La división de numeroA y numeroB es: ", divisionNumeros)
+
+// concatenaciones
+let textoUno = "Guadalupe"
+let textoDos = "Costa Martorel"
+
+let sumaDeTextos = textoUno + " " + textoDos
+console.log(sumaDeTextos)
+
+// prompt
+let nombreCompleto = prompt("Ingresá tu nombre completo:")
+console.log("Bienvenido", nombreCompleto)
+
+let dniIngresado = prompt("Ingresá tu DNI:")
+console.log("El DNI ingresado es:", dniIngresado)
+
+// alert
+alert("Hola " + nombreCompleto + ", tu DNI es " + dniIngresado)
+
+// confirm - true = verdadero o false = falso
+
+let edadIngresada = confirm("Sos mayor de edad?") */
+
+// parseInt - convertir a numeros enteros
+let numeroUno = parseInt(prompt("Ingresá un número:"))
+let numeroDos = parseInt(prompt("Ingresá otro número:"))
+
+let sumaNumeros = numeroUno + numeroDos
